84c4b5725d85bcf1fa35a0df68d259bdef8f35f6
MRandyAnugerah <improud.ann@gmail.com>
2023-05-21 14:56:35 +0800

    edit landing page

--- components/js/index.js
+++ components/js/index.js
@@ -3,7 +3,7 @@ function imgSlider(anything){
 }
 
 function changeColorCircle(color) {
-    const circles = document.querySelectorAll('.circle-top, .circle-bottom');
+    const circles = document.querySelectorAll('.circle-top, .circle-bottom, .name');
     circles.forEach(circle => {
         circle.style.background = color;
     });
